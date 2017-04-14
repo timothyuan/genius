933366980638b0953c25c4b7629323032da0991d
timothyuan <timothyuan@gmail.com>
2017-04-13 17:30:05 -0700

    android client code for file upload

---
+++ AsyncHttpClient.java
@@ -0,0 +1,28 @@
+import com.loopj.android.http.*;
+import java.io.*;
+import cz.msebera.android.httpclient.*;
+
+public class AsyncHttpClient{
+	public static void main(String[] args) {
+	
+	try {
+	File file = new File("/path/to/file.png");
+	RequestParams params = new RequestParams();
+    params.put("file", file);
+	AsyncHttpClient client = new AsyncHttpClient();
+	client.post("/upload", params, new AsyncHttpResponseHandler() {
+
+					@Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                        // called when response HTTP status is "200 OK"
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                        // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                    }
+	});
+	} catch(FileNotFoundException e) {}
+	}
+}
+
